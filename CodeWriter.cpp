26dd537ea3705e90d2ac4c49b5cf39a91f4c21c3
shinmy <shinmy.mt555@gmail.com>
2020-02-15 02:15:52 +0900

    make Code Writer but not comlete yet

---
+++ CodeWriter.cpp
@@ -0,0 +1,16 @@
+#include "CodeWriter.h"
+#include <fstream>
+#include <string>
+
+CodeWriter::CodeWriter(std::ofstream *fp) { ofp = fp; }
+void CodeWriter::setFileName(std::string fileName){ }
+void CodeWriter::writeArithemetic(std::string command){
+  
+}
+void CodeWriter::wtirePushPop(std::string comannd, std::string segment,
+                                     int index){
+
+                                     }
+void CodeWriter::close(){
+
+}
--- CodeWriter.h
+++ CodeWriter.h
@@ -1,9 +1,14 @@
+#include <fstream>
 #include <string>
 
-class CodeWrite {
-  CodeWrite();
-  std::string setFileName();
-  std::string writeArithemetic();
-  std::string wtirePushPop();
+class CodeWriter {
+ private:
+  std::ofstream *ofp;
+
+ public:
+  CodeWriter(std::ofstream *fp);
+  std::string setFileName(std::string fileName);
+  std::string writeArithemetic(std::string command);
+  std::string wtirePushPop(std::string comannd, std::string segment, int index);
   void close();
 };
